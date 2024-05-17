82fa4d5498108bc18cb72f689bf2b4b55421d7ce
LegoBuzz5697 <jarrettjake083@gmail.com>
2024-05-17 13:12:17 +0000

    smth

--- static/js/proxy.js
+++ static/js/proxy.js
@@ -1,8 +1,13 @@
 async function _main() {
-    document.getElementById('p').innerText = 'run';
-    if (!navigator.serviceWorker) throw new Error();
-    document.getElementById('p').innerText = 'run 2';
-    //await navigator.serviceWorker.register(__uv$config.sw);
-    document.getElementById('frame').src = __uv$config.prefix + location.href.split('=')[1];
+    try {
+       document.getElementById('p').innerText = 'run';
+        if (!navigator.serviceWorker) throw new Error();
+        document.getElementById('p').innerText = 'run 2';
+        //await navigator.serviceWorker.register(__uv$config.sw);
+        document.getElementById('frame').src = __uv$config.prefix + location.href.split('=')[1]; 
+    }
+    catch (err) {
+        document.getElementById('p').innerText = err;
+    }
 }
 _main();
