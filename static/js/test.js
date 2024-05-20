0ef47d3efa274d4c4b399f3f60520846196dc903
LegoBuzz5697 <jarrettjake083@gmail.com>
2024-05-20 15:33:11 +0000

    stuff

---
+++ static/js/test.js
@@ -0,0 +1,9 @@
+function execute(x) {
+    const invoke = (x) => { }
+    const getCookie = (x) => { }
+    const setCookie = (x) => { }
+    const stage = (x) => { }
+    const contentDoc = () => {}
+
+    var string = "'use strict' " + x;
+}
