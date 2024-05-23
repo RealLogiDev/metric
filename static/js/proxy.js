4ae9202d0e528180d83ccd6b5728af2d05f087f9
LegoBuzz5697 <jarrettjake083@gmail.com>
2024-05-23 15:31:17 +0000

    cluh

--- static/js/proxy.js
+++ static/js/proxy.js
@@ -1,7 +1,9 @@
 async function _main() {
     if (!navigator.serviceWorker) throw new Error('service workers arent supported');
     await navigator.serviceWorker.register('/uv/sw.js');
-    document.getElementById('frame').src = ('/uv/service/') + location.href.split('=')[1]; 
+    const frame = document.getElementById('frame')
+    frame.src = ('/uv/service/') + location.href.split('=')[1]; 
+    frame.appendChild((document.))
     window.history.pushState('proxy.html', 'cluh', '/proxy.html');
 }
 _main();
--- static/js/test.js
+++
@@ -1,9 +0,0 @@
-function execute(x) {
-    const invoke = (x) => { }
-    const getCookie = (x) => { }
-    const setCookie = (x) => { }
-    const stage = (x) => { }
-    const contentDoc = () => {}
-
-    var string = "'use strict' " + x;
-}
